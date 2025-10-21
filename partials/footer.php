8c50a317e6b5787afdbbe894333fd717a1e2a05e
raven-days <tthiraignesia@gmail.com>
2025-10-21 20:25:01 +0700

    Perbaiki Quick Links di Footer

--- partials/footer.php
+++ partials/footer.php
@@ -8,7 +8,7 @@
                 <h1 class="m-0 display-5 font-weight-semi-bold">Effort Outdoor</h1>
                 </a>
                 <p class="mt-3">Pakaian outdoor bekas berkualitas dengan harga terjangkau untuk pecinta alam.</p>
-                <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>Cilangkap Tapos Depok</p>
+                <p class="mb-2"><a href="https://www.google.com/maps/place/effort_outdoor/@-6.4468888,106.8533435,832m/data=!3m2!1e3!4b1!4m6!3m5!1s0x2e69ebd92e0ea4ef:0x5b33f047b30548a4!8m2!3d-6.4468888!4d106.8533435!16s%2Fg%2F11n0_xbpmd?entry=ttu&g_ep=EgoyMDI1MTAxNC4wIKXMDSoASAFQAw%3D%3D" target="_blank" class="text-dark"><i class="fa fa-map-marker-alt text-primary mr-3"></i>Cilangkap Tapos Depok</a></p>
                 <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>effortoutdoor@gmailo.com</p>
                 <p class="mb-0"><i class="fa fa-phone-alt text-primary mr-3"></i>+012 345 67890</p>
             </div>
@@ -19,29 +19,26 @@
                         <div class="d-flex flex-column justify-content-start">
                             <a class="text-dark mb-2" href="index.php"><i class="fa fa-angle-right mr-2"></i>Home</a>
                             <a class="text-dark mb-2" href="shop.php"><i class="fa fa-angle-right mr-2"></i>Our Shop</a>
-                            <a class="text-dark mb-2" href="detail.php"><i class="fa fa-angle-right mr-2"></i>Shop Detail</a>
-                            <a class="text-dark mb-2" href="cart.php"><i class="fa fa-angle-right mr-2"></i>Shopping Cart</a>
-                            <a class="text-dark mb-2" href="checkout.php"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
-                            <a class="text-dark" href="contact.php"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
+                            <a class="text-dark mb-2" href="contact.php"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
+                            <a class="text-dark mb-2" href="faq.php"><i class="fa fa-angle-right mr-2"></i>FAQ</a>
+
                         </div>
                     </div>
                     <div class="col-md-4 mb-5">
                         <h5 class="font-weight-bold text-dark mb-4">Quick Links</h5>
                         <div class="d-flex flex-column justify-content-start">
-                            <a class="text-dark px-2" href="">
-                        <i class="fab fa-facebook-f"></i>
-                    </a>
-                    <a class="text-dark px-2" href="">
-                        <i class="fab fa-twitter"></i>
+                            <a class="text-dark px-2" href="http://wa.me/628817589044" target="_blank">
+                        <p class="mb-2"><i class="fab fa-whatsapp"></i> WhatsApp</p>
                     </a>
                     <a class="text-dark px-2" href="https://www.instagram.com/effort_outdoor/?igsh=a3d3ZXV3d3o1b2Jv#">
-                        <i class="fab fa-instagram"></i>
+                        <p class="mb-2"><i class="fab fa-instagram"></i> Instagram</p>
                     </a>
-                    <a class="text-dark pl-2" href="https://shopee.co.id/effort_outdoor" target="_blank">
+                    <a class="text-dark pl-1" href="https://shopee.co.id/effort_outdoor" target="_blank">
                         <img src="https://e7.pngegg.com/pngimages/664/195/png-clipart-shopee-indonesia-android-android-text-rectangle-thumbnail.png"
                             alt="Shopee"
-                            style="width: 25px; height: 25px;">
+                            style="width: 20px; height: 20px;"> Shoppe
                         </a>
+                        
                         </div>
                     </div>
                     <div class="col-md-4 mb-5">
