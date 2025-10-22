f2b51bcd09e266b66a78d863881804d53a91c49a
raven-days <tthiraignesia@gmail.com>
2025-10-22 17:17:44 +0700

    perbaiki navbar agar konsisten

--- partials/navbar.php
+++ partials/navbar.php
@@ -8,7 +8,7 @@
                 <nav class="collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 border border-top-0 border-bottom-0 bg-light" id="navbar-vertical" style="width: calc(100% - 30px); z-index: 1;">
                     <div class="navbar-nav w-100 overflow-hidden" style="height: 410px">
                         <div class="nav-item dropdown">
-                            <a href="#" class="nav-link" data-toggle="dropdown">celana Panjang <i class="fa fa-angle-down float-right mt-1"></i></a>
+                            <a href="#" class="nav-link" data-toggle="dropdown">Celana Panjang <i class="fa fa-angle-down float-right mt-1"></i></a>
                             <div class="dropdown-menu position-absolute bg-secondary border-0 rounded-0 w-100 m-0">
                                 <a href="shop.php?category=celana-panjang&gender=men" class="dropdown-item" class="dropdown-item">Men's</a>
                                 <a href="shop.php?category=celana-panjang&gender=women" class="dropdown-item" class="dropdown-item">Women's</a>
@@ -36,8 +36,9 @@
             </div>
             <div class="col-lg-9">
                 <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0">
-                    <a href="" class="text-decoration-none d-block d-lg-none">
-                        <h1 class="m-0 display-5 font-weight-semi-bold"><span class="text-primary font-weight-bold border px-3 mr-1">E</span>Shopper</h1>
+                    <a href="" class="text-decoration-none d-flex align-items-center d-block d-lg-none">
+                        <img src="img/logo.jpg" alt="Logo" style="height: 40px; width: auto;" class="mr-1 border" />
+                        <h1 class="m-0 display-5 font-weight-semi-bold">Effort Outdoor</h1>
                     </a>
                     <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                         <span class="navbar-toggler-icon"></span>
